16957c53222f38e090377eb2271fefaa02105f40
Hernan Y.Ke <yuriqiao@gmail.com>
2015-10-25 13:37:16 -0400

    5.1 5.2 5.5 7.1

---
+++ C7_1/server.js
@@ -0,0 +1,34 @@
+var express = require('express'),
+    app = express(),
+    http = require('http'),
+    socketIO = require('socket.io'),
+    fs = require('fs'),
+    path = require('path'),
+    server, io;
+
+app.get('/', function (req, res) {
+    res.sendFile(__dirname + '/index.html');
+});
+
+server = http.Server(app);
+server.listen(5000);
+
+io = socketIO(server);
+
+io.on('connection',function(socket){
+    var readStream = fs.createReadStream(path.resolve(__dirname,'./woodchuck.jpg'),{encoding:'binary'}),chunks=[];
+    var delay = 0;
+
+    readStream.on('readable', function(){
+        console.log('loading');
+    });
+
+    readStream.on('data',function(chunk){
+        chunks.push(chunk);
+        socket.emit('img-chunk',chunk);
+    });
+
+    readStream.on('end',function(){
+        console.log('end');
+    })
+})
